9dd211f14c0c23aa82473bf0a0c1c2afeaa7b549
caioaugustoo <caioamfr@gmail.com>
2023-02-25 20:03:05 -0300

    feat: update crypto package

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -3,31 +3,63 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"encoding/hex"
-	"log"
+	"fmt"
+	"io"
 
 	"github.com/mergermarket/go-pkcs7"
 )
 
-// Decrypt decrypts an string.
-func Decrypt(encrypted, key string) (string, error) {
+// Special thanks to https://gist.github.com/brettscott/2ac58ab7cb1c66e2b4a32d6c1c3908a7 ❤️
+
+// Encrypt encrypts plain text string into cipher text string.
+func Encrypt(unencrypted, cipherKey string) (string, error) {
+	key := []byte(cipherKey)
+	plainText := []byte(unencrypted)
+	plainText, err := pkcs7.Pad(plainText, aes.BlockSize)
+	if err != nil {
+		return "", fmt.Errorf(`plainText: "%s" has error`, plainText)
+	}
+	if len(plainText)%aes.BlockSize != 0 {
+		err := fmt.Errorf(`plainText: "%s" has the wrong block size`, plainText)
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(cipherText[aes.BlockSize:], plainText)
+
+	return fmt.Sprintf("%x", cipherText), nil
+}
+
+// Decrypt decrypts cipher text string into plain text string
+func Decrypt(encrypted, cipherKey string) (string, error) {
+	key := []byte(cipherKey)
 	cipherText, _ := hex.DecodeString(encrypted)
 
-	block, err := aes.NewCipher([]byte(key))
-
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
 
 	if len(cipherText) < aes.BlockSize {
-		log.Fatalln("cipherText too short")
+		panic("cipherText too short")
 	}
-
 	iv := cipherText[:aes.BlockSize]
 	cipherText = cipherText[aes.BlockSize:]
-
 	if len(cipherText)%aes.BlockSize != 0 {
-		log.Fatalln("cipherText is not a multiple of the block size")
+		panic("cipherText is not a multiple of the block size")
 	}
 
 	mode := cipher.NewCBCDecrypter(block, iv)
