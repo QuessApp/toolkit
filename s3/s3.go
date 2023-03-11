a766ce0320689f518202dbfea05e100122226e2c
caioaugustoo <caioamfr@gmail.com>
2023-03-11 00:05:33 -0300

    refactor: update order of params

--- s3/s3.go
+++ s3/s3.go
@@ -31,7 +31,7 @@ func Configure(region *string, S3credentials *S3Credentials) (*s3.S3, error) {
 }
 
 // UploadFile uploads file to S3.
-func UploadFile(client *s3.S3, fileName, bucketName string, file io.ReadSeeker, acl *string) (*s3.PutObjectOutput, error) {
+func UploadFile(client *s3.S3, bucketName, fileName string, file io.ReadSeeker, acl *string) (*s3.PutObjectOutput, error) {
 	return client.PutObject(&s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
 		Key:    aws.String(fileName),
@@ -41,7 +41,7 @@ func UploadFile(client *s3.S3, fileName, bucketName string, file io.ReadSeeker, 
 }
 
 // DeleteFile deletes file from S3.
-func DeleteFile(client *s3.S3, fileName, bucketName string) (*s3.DeleteObjectOutput, error) {
+func DeleteFile(client *s3.S3, bucketName, fileName string) (*s3.DeleteObjectOutput, error) {
 	return client.DeleteObject(&s3.DeleteObjectInput{
 		Bucket: aws.String(bucketName),
 		Key:    aws.String(fileName),
