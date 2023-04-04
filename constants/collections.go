01e4bc1859330ba27ef9ce376717ebf95c3ba594
caioaugustoo <caioamfr@gmail.com>
2023-04-04 16:44:34 -0300

    chore: add users bans collection

--- constants/collections.go
+++ constants/collections.go
@@ -1,9 +1,10 @@
 package constants
 
 const (
-	USERS     = "users"
-	QUESTIONS = "questions"
-	BLOCKS    = "blocks"
-	TOKENS    = "tokens"
-	REPORTS   = "reports"
+	USERS      = "users"
+	QUESTIONS  = "questions"
+	BLOCKS     = "blocks"
+	TOKENS     = "tokens"
+	REPORTS    = "reports"
+	USERS_BANS = "users_bans"
 )
